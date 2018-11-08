cc6b02d4ebe2e7c376d8de8f3ff80507832c634e
root <root@ip-10-87-20-137.ap-southeast-2.compute.internal>
2018-11-08 06:25:03 +0000

    Adding CloudWatch support to our application

--- helloworld.js
+++ helloworld.js
@@ -1,4 +1,24 @@
 var http = require("http")
+var winston = require("winston")
+var AWS = require("aws-sdk")
+
+AWS.config.update({region:"ap-southeast-2"})
+var cwevents = new AWS.CloudWatchEvents({apiVersion: "2015-10-07"})
+var cw = new AWS.CloudWatch({apiVersion: "2010-08-01"})
+
+var version = process.env.HELLOWORLD_VERSION 
+var logger = new winston.Logger({ 
+  transports: [new winston.transports.Console({ 
+    timestamp: function() { 
+       var d = new Date()
+       return d.toISOString()
+    }, 
+  })] 
+}) 
+logger.rewriters.push(function(level, msg, meta) { 
+  meta.version = version 
+  return meta 
+}) 
 
 
 
@@ -11,6 +31,25 @@ http.createServer(function (request, response) {
    // HTTP Status: 200 : OK
 
    // Content Type: text/plain
+  var event ={ 
+      Entries: [{ 
+        Detail: JSON.stringify(request.headers), 
+        DetailType: "hellworld application access request", 
+        Source: "helloworld.app" 
+      }] 
+    }
+  var metric = { 
+      MetricData: [{ 
+        MetricName: "page_viewwed",
+	Dimensions: [{ 
+          Name: "Version", 
+          Value: version 
+        }], 
+        Unit: "None", 
+        Value: 1.0 
+      }], 
+      Namespace: "Helloworld/traffic" 
+    }
 
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
@@ -18,7 +57,22 @@ http.createServer(function (request, response) {
 
    // Send the response body as "Hello World"
 
-   response.end('Hello World\n')
+   response.end('Hello New World\n')
+
+   cwevents.putEvents(event, function(err, data) { 
+    if (err) { 
+      logger.error("error", "an error occurred when creating an event", {error: err}) 
+    } else { 
+      logger.info("created event", {entries: data.Entries}) 
+    } 
+  }) 
+  cw.putMetricData(metric, function(err, data) { 
+    if (err) { 
+      logger.error("an error occurred when creating a metric", {error: err}); 
+    } else { 
+      logger.info("created metric", {data: JSON.stringify(data)}); 
+    } 
+  })
 
 }).listen(3000)
 
@@ -26,4 +80,4 @@ http.createServer(function (request, response) {
 
 // Console will print the message
 
-console.log('Server running')
+logger.info('Server running')
